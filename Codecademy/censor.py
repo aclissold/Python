a873d587caf67ff641e256ff7f946402bcb5fb66
Andrew Clissold <ajclisso@oakland.edu>
2013-06-13 08:46:22 -0400

    Solved censor.py

--- Codecademy/censor.py
+++ Codecademy/censor.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
 def censor(text, censorWord):
     sentence = ''               # Used to recombine wordList
     wordList = text.split(" ")  # Make a List of words
-    for word in wordList:
+    for index, word in enumerate(wordList):
         if word == censorWord:
             word = '*' * len(word)  # Replace words that should be censored
-            print word
+            wordList[index] = word
     for word in wordList:
         sentence += word + ' '
     sentence = sentence.strip()
     return sentence
+
+if __name__ == '__main__':
+    print(censor("hey hey hey", "hey"))
