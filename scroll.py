80b31cbddede2bd68a20198f9e9fad174ad6a6b3
Andrew Clissold <ajclisso@oakland.edu>
2013-07-08 14:08:02 -0400

    Created scrolling Spotify info module

--- scroll.py
+++ scroll.py
@@ -1,14 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
+
+"""Runs in the background on my Awesome setup to display Spotify info.
+
+Uses deprecated Python 2 techniques.
+
+"""
+
 import os
+import commands
+import subprocess
 from time import sleep
-song = 'Man on Fire ~ Edward Sharpe & the Magnetic Zeros'
-spaces = ' ' * len(song)
-while True:
-    for i in range(len(song)):
-        os.system('clear')
-        print(song[i:] + spaces[:i])
-        sleep(1 / 10)
-    for i in range(len(song)):
-        os.system('clear')
-        print(spaces[i:] + song[:i])
-        sleep(1 / 10)
+
+if __name__ == '__main__':
+    spaces = ' ' * 20
+    while True:
+        song = commands.getstatusoutput('spotout.sh')[1]
+        if song != 'SPOTIFY' and song != '':
+            song = spaces + song + spaces
+            song = song.replace('&', '+')
+            song = song.replace("'", "`")
+            for i in range(len(song) - 20):
+                display = song[i:i+20]
+                subprocess.call(['echo \'spotwidget.text = " ' + display + ' "\' | awesome-client'], shell=True)
+                sleep(0.3)
+                if i == 20:
+                    sleep(2.7)
+        else:
+            display = ''
+            subprocess.call(['echo \'spotwidget.text = "' + display + '"\' | awesome-client'], shell=True)
+            sleep(10)
