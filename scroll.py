25648ab0c191f90a5d1f703e7f1225cb635b5e29
Andrew Clissold <andrewclissold@gmail.com>
2013-10-08 09:45:50 -0400

    scroll.py: fix issue with quotation marks terminating the string

--- scroll.py
+++ scroll.py
@@ -1,36 +1,45 @@
-#!/usr/bin/python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Runs in the background on my Awesome setup to display Spotify info.
+"""Runs in the background on my Awesome setup to display Spotify info."""
 
-Uses deprecated Python 2 techniques.
-
-"""
-
-import os
-import commands
 import subprocess
+import sys
 from time import sleep
 
 if __name__ == '__main__':
     spaces = ' ' * 20
     while True:
-        song = commands.getstatusoutput('spotout.sh')[1]
-        if song != 'SPOTIFY' and song != '':
+        try:
+            song = subprocess.check_output(['/home/ajclisso/Code/Python/PySpotifyInfo/spotify_control.py', '-d', 'title', 'artist', '-m', ' ~ '], universal_newlines=True)
             song_with_spaces = spaces + song + spaces
             song_with_spaces = song_with_spaces.replace('&', '+')
             song_with_spaces = song_with_spaces.replace("'", "`")
             song_with_spaces = song_with_spaces.replace("ä", "a")
+            song_with_spaces = song_with_spaces.replace("\"", "`")
+
             for i in range(len(song_with_spaces) - 20):
-                if song != commands.getstatusoutput('spotout.sh')[1]:
-                    print("Failing! song, spotout.sh:")
+                if song != subprocess.check_output(['/home/ajclisso/Code/Python/PySpotifyInfo/spotify_control.py', '-d', 'title', 'artist', '-m', ' ~ '], universal_newlines=True):
+                    print("Failing! Contents of song:", song)
                     break
                 display = song_with_spaces[i:i+20]
                 subprocess.call(['echo \'spotwidget.text = " ' + display + ' "\' | awesome-client'], shell=True)
                 sleep(0.3)
                 if i == 20:
                     sleep(2.7)
-        else:
+
+        except subprocess.CalledProcessError:
+            try:
+                # Spotify is not running
+                display = ''
+                subprocess.call(['echo \'spotwidget.text = "' + display + '"\' | awesome-client'], shell=True)
+                sleep(10)
+            except KeyboardInterrupt:
+                display = ''
+                subprocess.call(['echo \'spotwidget.text = "' + display + '"\' | awesome-client'], shell=True)
+                sys.exit()
+
+        except KeyboardInterrupt:
             display = ''
             subprocess.call(['echo \'spotwidget.text = "' + display + '"\' | awesome-client'], shell=True)
-            sleep(10)
+            sys.exit()
